ac35f9cdba9ba1df72ed09112236385a45659281
LuisaMG01 <lmalvarez8@eafit.edu.co>
2024-04-09 10:45:47 -0500

    separating functions

--- src/actions/subscribe.c
+++ src/actions/subscribe.c
@@ -155,3 +155,26 @@ void addSubscriber(TopicNode *node, const int subscriber)
     node->subscribers[node->num_subscribers] = subscriber;
     node->num_subscribers++;
 }
+
+uint16_t get_packet_id() {
+    static uint16_t packet_id;
+    static bool init = false;
+
+    if(init == false)
+    {
+        packet_id = 0;
+        init = true;
+    }
+    else
+    {
+        packet_id++;
+    }
+
+    if (packet_id > 65535) {
+        packet_id = 1;
+    }
+
+    uint16_t msb = (packet_id >> 8) & 0xFF;
+    uint16_t lsb = packet_id & 0xFF;
+    return (msb << 8) | lsb;
+}
